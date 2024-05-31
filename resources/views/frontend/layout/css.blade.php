12768bcb87bbfa5a0768da5c2f5a90cc3a6c7474
yuzgec <olcayy@gmail.com>
2024-05-31 18:00:47 +0300

    Fix

--- resources/views/frontend/layout/css.blade.php
+++ resources/views/frontend/layout/css.blade.php
@@ -11,7 +11,7 @@
 <link rel="stylesheet" href="/frontend/css/skins/skin-cleaning-services.css?ver=4">
 <link id="skinCSS" rel="stylesheet" href="/frontend/css/color.css?ver=4">
 
-       <script> gtag('event', 'conversion', {'send_to': 'AW-969497658/5B5CCJDn_ZUYELq4pc4D'}); </script>
+       <script> gtag('event', 'conversion', {'send_to': 'GTM-KRSV9JZ7/tEJKCL3Sx7UZELq4pc4D'}); </script>
 
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-KRSV9JZ7"></script>
@@ -24,21 +24,6 @@
 
         </script>
 
-
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-       })(window,document,'script','dataLayer','GTM-KRSV9JZ7');</script>
-       <!-- End Google Tag Manager -->
-
-       <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KRSV9JZ7"
-       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-       <!-- End Google Tag Manager (noscript) -->
-
-       GTM-KRSV9JZ7
 
        <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-0RL2BZLKH0"></script>
